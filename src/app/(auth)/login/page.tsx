396cb07e11028b5bb59693b1b633b2666b09bc1a
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-03-20 20:33:44 -0300

    update(login): base da tela de login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,29 @@
+import Image from "next/image";
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-full h-screen flex">
+      <div className="bg-brand-600 text-white w-[35%] h-full flex flex-col items-center justify-between p-4">
+        <div className="w-full flex items-start">
+          <Image
+            src="/logo_educacao_w.svg"
+            width={240}
+            height={90}
+            alt="Logo da Prefeitura Municipal de Caraguatatuba"
+          />
+        </div>
+        <div>
+          <form method="post">
+            
+          </form>
+        </div>
+        <div className="text-center text-sm">
+          <span>© 2026 Prefeitura Municipal de Caraguatatuba.<br />Todos os direitos reservados.</span>
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
