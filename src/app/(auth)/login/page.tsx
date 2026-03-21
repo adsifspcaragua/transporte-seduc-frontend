eebbed400e84d0b2f7cf132640e13952f5eff058
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-03-20 22:11:19 -0300

    update(login): implementação do carrossel

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import LoginCarousel from "@/components/auth/LoginCarousel";
 
 export default function Login() {
   return (
-    <div className="w-full h-screen flex">
-      <div className="bg-brand-600 text-white w-[35%] h-full flex flex-col items-center justify-between p-4">
-        <div className="w-full flex items-start">
+    <div className="flex h-screen w-full overflow-hidden">
+      <div className="flex h-full w-[575px] shrink-0 flex-col justify-between bg-brand-600 p-6 text-white">
+        <div className="w-full">
           <Image
             src="/logo_educacao_w.svg"
             width={240}
@@ -12,17 +13,37 @@ export default function Login() {
             alt="Logo da Prefeitura Municipal de Caraguatatuba"
           />
         </div>
-        <div>
-          <form method="post">
-            
-          </form>
+
+        <div className="mx-auto flex w-full max-w-[450px] flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            {/* Logo do sistema virá aqui */}
+            <h1 className="text-2xl font-semibold text-center">
+              Acesso ao sistema
+            </h1>
+
+            <span className="text-lg leading-relaxed text-white">
+              Gerencie e acompanhe o transporte universitário de forma simples e segura.
+            </span>
+          </div>
+
+          <div>
+            <form method="post">
+              {/* Campos virão aqui */}
+            </form>
+          </div>
         </div>
-        <div className="text-center text-sm">
-          <span>© 2026 Prefeitura Municipal de Caraguatatuba.<br />Todos os direitos reservados.</span>
+
+        <div className="text-center text-sm text-white">
+          <span>
+            © 2026 Prefeitura Municipal de Caraguatatuba.
+            <br />
+            Todos os direitos reservados.
+          </span>
         </div>
       </div>
-      <div>
 
+      <div className="h-full w-full">
+        <LoginCarousel />
       </div>
     </div>
   );
