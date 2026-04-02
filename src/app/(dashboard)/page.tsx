8f963c9809c58ec5e4647f22f6d1f0a172c89bae
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-04-02 14:00:56 -0300

    add(dashboard): implementa funcionalidade de logout com feedback de carregamento

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,8 +1,39 @@
+'use client';
+
+import { useState } from "react";
+import { FiLogOut } from "react-icons/fi";
+
+import Button from '@/components/ui/Button';
+import { useAuth } from '@/features/auth/hooks/use-auth';
 
 export default function Dashboard() {
+  const { signOut } = useAuth();
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogout() {
+    try {
+      setLoading(true);
+      await signOut();
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <Button
+        type="button"
+        variant="ghost"
+        size="sm"
+        uppercase={false}
+        fullWidth={false}
+        loading={loading}
+        leftIcon={<FiLogOut />}
+        onClick={handleLogout}
+      >
+        Sair
+      </Button>
     </div>
   );
 }
